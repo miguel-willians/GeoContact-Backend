20782a32252eedb8d2a9d999909b4d12fba50d50
Miguel Willians <136731765+miguel-willians@users.noreply.github.com>
2025-05-09 14:38:05 -0300

    chore:
    criar base do projeto no initializr

---
+++ geocontact/src/main/java/com/example/geocontact/GeocontactApplication.java
@@ -0,0 +1,13 @@
+package com.example.geocontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocontactApplication.class, args);
+	}
+
+}
---
+++ geocontact/src/test/java/com/example/geocontact/GeocontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geocontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
