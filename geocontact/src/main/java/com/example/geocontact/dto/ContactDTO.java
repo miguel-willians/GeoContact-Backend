63a2284ecbc5bde2c28527f2b26f2d3ff240e8a1
miguel-willians <miiguel.wso@gmail.com>
2025-05-15 21:01:49 -0300

    feat: criação dos DTOs

---
+++ geocontact/src/main/java/com/example/geocontact/dto/ContactDTO.java
@@ -0,0 +1,93 @@
+package com.example.geocontact.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class ContactDTO {
+    private Long id;
+    @NotBlank private String name;
+    @NotBlank private String cpf;
+    @NotBlank private String phone;
+
+    @NotBlank private String street;
+    @NotBlank private String number;
+    private String complement;
+    @NotBlank private String city;
+    @NotBlank private String state;
+    @NotBlank private String cep;
+    @NotNull private Double latitude;
+    @NotNull private Double longitude;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getComplement() {
+        return complement;
+    }
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
