2aecf218a7cc1f7942ad7b1747a9f848dff1f9dd
miguel-willians <miiguel.wso@gmail.com>
2025-06-15 12:50:49 -0300

    fix: ajustar repositórios

--- geocontact/src/main/java/com/example/geocontact/repository/ContactRepository.java
+++ geocontact/src/main/java/com/example/geocontact/repository/ContactRepository.java
@@ -1,14 +1,17 @@
 package com.example.geocontact.repository;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.geocontact.entity.Contact;
 import com.example.geocontact.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-    List<Contact> findByUserAndNameContainingIgnoreCaseOrCpfContaining(User user, String name, String cpf);
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
+
+    List<Contact> findByUser(User user);
+
+    List<Contact> findByUserAndNameContainingIgnoreCaseOrUserAndCpfContaining(User user1, String name, User user2, String cpf);
+
     boolean existsByUserAndCpf(User user, String cpf);
 }
--- geocontact/src/main/java/com/example/geocontact/repository/UserRepository.java
+++ geocontact/src/main/java/com/example/geocontact/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.geocontact.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
